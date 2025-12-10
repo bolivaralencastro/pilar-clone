f576248dc9c7de9dc6e981edaaa6061d42547aba
Bolivarlencastro <bolivar.alencastro@keeps.com.br>
2025-12-09 21:57:54 -0300

    feat: add favicon using logo-icon-mark.svg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,7 @@ export default defineNuxtConfig({
         { name: 'description', content: 'Clone do sistema de busca de imóveis Pilar Homes' }
       ],
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo-icon-mark.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Space+Mono:wght@400;700&family=Roboto:wght@300;400;500;700&display=swap' },
